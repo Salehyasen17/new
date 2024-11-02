ae884b39906d260a375d776eec271b6ccc3a936a
Salehyasen17 <salehyasen17@gmail.com>
2024-11-02 18:03:10 +0200

    hjkl

--- app/src/main/java/com/example/mmmm/MainActivity.java
+++ app/src/main/java/com/example/mmmm/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.mmmm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int year;
+    String name, details;
     TextView text1, text2;
     EditText et1, et2;
     @Override
@@ -15,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text1 = findViewById(R.id.textView);
         text2 = findViewById(R.id.textView2);
+        et1 = findViewById(R.id.etName);
+        et2 = findViewById(R.id.etYear);
+    }
+
+    public void showDetails(View view){
+        if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please fill in fields", Toast.LENGTH_LONG).show();
+        }
+        else {
+            name = et1.getText().toString();
+            year = Integer.parseInt(et2.getText().toString());
+            int age = 2024 - year;
+            details = "Welcome "+name+ "\nYour age is "+age+" years";
+            text1.setText(details);
+        }
 
     }
 }
